7d1a963a1c272c4b128b3ce5aed58145f020a0e0
Ethan <easonbest123@gmail.com>
2023-12-11 10:08:08 +0800

    mvim更新 顯示 刪除 功能完成

--- back/mvim.php
+++ back/mvim.php
@@ -10,20 +10,21 @@
                     <td></td>
                 </tr>
                 <?php
-                $rows=$Title->all();
+                $rows=$Mvim->all();
                 foreach($rows as $row){
                 ?>
                 <tr>
-                    <td width="70%">
-                        <img src="./img/<?=$row['img'];?>" style="width:80%;height:30px">
+                    <td>
+                        <img src="./img/<?=$row['img'];?>" style="width:150px;height:100px">
                     </td>
-                    <td width="10%">
-                        <input type="radio" name="sh" value="<?=$row['id'];?>">
+                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">                                    
+                    <td>
+                    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>>
                     </td>
-                    <td width="10%">
-                    <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                    <td>
+                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
                     </td>
-                    <td width="10%">
+                    <td>
                     <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換動畫">
                     </td>
                 </tr>
--- modal/upload.php
+++ modal/upload.php
@@ -1,33 +1,44 @@
-<?php
+<?php 
 switch($_GET['table']){
     case "title":
-    echo "<td>更新網站標題圖片</td>";
+        echo "<h3>更新網站標題圖片</h3>";
     break;
-    case "mvim":
-    echo "<td>更換動畫圖片</td>";
+    case 'mvim':
+        echo "<h3>更換動畫圖片</h3>";
     break;
-    case "image":
-    echo "<td>校園映像圖片</td>";
+    case 'image':
+        echo "<h3>更新校園映像圖片</h3>";
     break;
-    
+
 }
 ?>
 
-
-<h3>更新網站標題圖片</h3>
-<h3>更換動畫圖片</h3>
 <hr>
 <form action="./api/update.php" method="post" enctype="multipart/form-data">
 <table>
     <tr>
-        <td>標題區圖片</td>
+    <?php 
+        switch($_GET['table']){
+            case "title":
+                echo "<td>標題區圖片</td>";
+            break;
+            case 'mvim':
+                echo "<td>動畫圖片</td>";
+            break;
+            case 'image':
+                echo "<td>校園映像圖片</td>";
+            break;
+        }
+    ?>
+        
         <td><input type="file" name="img" id=""></td>
     </tr>
-    </table>
-    <div>
-        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
-        <input type="hidden" name="id" value="<?=$_GET['id'];?>">
-        <input type="submit" value="更新">
-        <input type="reset" value="重置">
-    </div>
+</table>
+<div>
+    <input type="hidden" name="table" value="<?=$_GET['table'];?>">
+    <input type="hidden" name="id" value="<?=$_GET['id'];?>">
+    <input type="submit" value="更新">
+    <input type="reset" value="重置">
+</div>
+
 </form>
