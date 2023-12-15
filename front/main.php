484c16cba89b5005f5813005c83894fcff4b9a49
Ethan <easonbest123@gmail.com>
2023-12-15 14:41:33 +0800

    首頁最新消息顯示

--- front/main.php
+++ front/main.php
@@ -14,7 +14,7 @@
             $lins=$Mvim->all(['sh'=>1]);
              foreach($lins as $lin){
                echo "lin.push('{$lin['img']}');";
-			   
+
              }
             ?>
 
@@ -37,6 +37,14 @@
         <span class="t botli">最新消息區
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+		<?php
+            $news=$News->all(['sh'=>1], "limit 5");
+			foreach($news as $n){
+				echo "<li>";
+				echo mb_substr($n['text'],0,20);
+				echo "...</li>";
+			}
+		?>
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
