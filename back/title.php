4a1cf31f33414a5d07181242067cc02eed225b21
Ethan <easonbest123@gmail.com>
2023-12-15 16:40:59 +0800

    最新消息顯示區完成

--- back/title.php
+++ back/title.php
@@ -23,7 +23,7 @@
                     </td>
                     <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     <td width="7%">
-                        <input type="radio" name="sh" value="<?=$row['id'];?>">
+                        <input type="radio" name="sh" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>>
                     </td>
                     <td width="7%">
                     <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
