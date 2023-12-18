9ee82bd606781e4a0b3b5fe3259eecd667dd4d5a
Ethan <easonbest123@gmail.com>
2023-12-18 10:14:02 +0800

    校園映像區 圖片變換功能完成

--- index.php
+++ index.php
@@ -87,16 +87,28 @@
 				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
+					<div class="cent" onclick="pp(1)"><img src="./icons/up.jpg" alt=""></div>
+					<?php
+                    $imgs=$Image->all(['sh' => 1]);
+					
+					foreach($imgs as $idx => $img){
+                    ?>
+						<div id="ssaa<?=$idx;?>" class="im cent">
+					     <img src="./img/<?=$img['img'];?>" style="width:150px;height:103px;border:3px solid orange;margin:3px">
+					    </div>
+					<?php
+					}
+                    ?>
+					<div class="cent" onclick="pp(2)"><img src="./icons/dn.jpg" alt=""></div>
 					<script>
-						var nowpage = 0,
-							num = 0;
-
+				      var nowpage=1,num=<?=$Image->count(['sh'=>1]);?>;
 						function pp(x) {
 							var s, t;
+							// 與分頁寫法類似
 							if (x == 1 && nowpage - 1 >= 0) {
 								nowpage--;
 							}
-							if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+							if (x == 2 && (nowpage + 1) * 3 <= num * 1 - 3) {
 								nowpage++;
 							}
 							$(".im").hide()
